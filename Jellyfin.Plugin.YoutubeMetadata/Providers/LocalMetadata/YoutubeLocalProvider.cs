60e4753c1ac965795f5459fa488c769d08356d48
Joel Goguen <jgoguen@users.noreply.github.com>
2021-09-04 08:01:21 -0700

    Add Music Video support for local .info.json files
    - Extends YTDLMovieJson to add fields for music videos
    - Adds new Utils function to create a MetadataResult for music videos
    - Abstracts code common between the existing local provider and the new
      music video provider
    
    Tested:
    - Adding a new music video without a .info.json allows the API process
      to work.
    - Log messages show the concrete class being used, not the abstract
      class.
    - Adding a new music video with a .info.json populates metadata fields
      upon refresh.

--- Jellyfin.Plugin.YoutubeMetadata/Providers/LocalMetadata/YoutubeLocalProvider.cs
+++ Jellyfin.Plugin.YoutubeMetadata/Providers/LocalMetadata/YoutubeLocalProvider.cs
@@ -1,86 +1,19 @@
-﻿using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Providers;
-using System.IO;
-using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
 using MediaBrowser.Model.IO;
 using Microsoft.Extensions.Logging;
 using MediaBrowser.Controller.Entities.Movies;
 
 namespace Jellyfin.Plugin.YoutubeMetadata.Providers
 {
-    public class YoutubeLocalProvider : ILocalMetadataProvider<Movie>, IHasItemChangeMonitor
+    public class YoutubeLocalProvider : AbstractYoutubeLocalProvider<YoutubeLocalProvider, Movie>
     {
-        private readonly ILogger<YoutubeLocalProvider> _logger;
-        private readonly IFileSystem _fileSystem;
+        public YoutubeLocalProvider(IFileSystem fileSystem, ILogger<YoutubeLocalProvider> logger) : base(fileSystem, logger) { }
 
-        public YoutubeLocalProvider(IFileSystem fileSystem, ILogger<YoutubeLocalProvider> logger)
+        public override string Name => "YouTube Local Metadata";
+
+        internal override MetadataResult<Movie> GetMetadataImpl(Utils.YTDLMovieJson jsonObj)
         {
-            _fileSystem = fileSystem;
-            _logger = logger;
+            return Utils.YTDLJsonToMovie(jsonObj);
         }
-
-        /// <summary>
-        /// Providers name, this appears in the library metadata settings.
-        /// </summary>
-        public string Name => "YouTube Local Metadata";
-
-        private FileSystemMetadata GetInfoJson(string path)
-        {
-            var fileInfo = _fileSystem.GetFileSystemInfo(path);
-            var directoryInfo = fileInfo.IsDirectory ? fileInfo : _fileSystem.GetDirectoryInfo(Path.GetDirectoryName(path));
-            var directoryPath = directoryInfo.FullName;
-            var specificFile = Path.Combine(directoryPath, Path.GetFileNameWithoutExtension(path) + ".info.json");
-            var file = _fileSystem.GetFileInfo(specificFile);
-            return file;
-        }
-
-        /// <summary>
-        /// Returns bolean if item has changed since last recorded.
-        /// </summary>
-        /// <param name="item"></param>
-        /// <param name="directoryService"></param>
-        /// <returns></returns>
-        public bool HasChanged(BaseItem item, IDirectoryService directoryService)
-        {
-            var infoJson = GetInfoJson(item.Path);
-            var result = infoJson.Exists && _fileSystem.GetLastWriteTimeUtc(infoJson) < item.DateLastSaved;
-            return result;
-        }
-
-        /// <summary>
-        /// Retrieves metadata of item.
-        /// </summary>
-        /// <param name="info"></param>
-        /// <param name="directoryService"></param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        public Task<MetadataResult<Movie>> GetMetadata(ItemInfo info, IDirectoryService directoryService, CancellationToken cancellationToken)
-        {
-            var result = new MetadataResult<Movie>();
-            if (Plugin.Instance.Configuration.DisableLocalMetadata)
-            {
-                _logger.LogInformation("Local Metadata Disabled");
-                result.HasMetadata = false;
-                return Task.FromResult(result);
-            }
-            try
-            {
-                var item = new Movie();
-                var infoJson = GetInfoJson(info.Path);
-                var jsonObj = Utils.ReadYTDLInfo(infoJson.FullName, cancellationToken);
-                result = Utils.YTDLJsonToMovie(jsonObj);
-            }
-            catch (FileNotFoundException)
-            {
-                _logger.LogInformation("Could not find {0}", info.Path);
-                result.HasMetadata = false;
-                return Task.FromResult(result);
-            }
-            return Task.FromResult(result);
-        }
-
-        
     }
 }
---
+++ Jellyfin.Plugin.YoutubeMetadata/Providers/LocalMetadata/YoutubeMusicLocalProvider.cs
@@ -0,0 +1,19 @@
+﻿using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.IO;
+using Microsoft.Extensions.Logging;
+
+namespace Jellyfin.Plugin.YoutubeMetadata.Providers
+{
+    public class YoutubeMusicLocalProvider : AbstractYoutubeLocalProvider<YoutubeMusicLocalProvider, MusicVideo>
+    {
+        public YoutubeMusicLocalProvider(IFileSystem fileSystem, ILogger<YoutubeMusicLocalProvider> logger) : base(fileSystem, logger) { }
+
+        public override string Name => "YouTube Music Local Metadata";
+
+        internal override MetadataResult<MusicVideo> GetMetadataImpl(Utils.YTDLMovieJson jsonObj)
+        {
+            return Utils.YTDLJsonToMusicVideo(jsonObj);
+        }
+    }
+}
