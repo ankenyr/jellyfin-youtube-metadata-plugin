42dcbbcdec3fc6a82379e13da7c382a66be20faa
ankenyr <ankenyr@gmail.com>
2022-01-04 15:08:00 -0800

    Adding a season provider for the local providers. This fixes a bug where a season is made for every folder. We will enforce a single season for any channel.

---
+++ Jellyfin.Plugin.YoutubeMetadata/Providers/LocalMetadata/YoutubeLocalSeasonProvider.cs
@@ -0,0 +1,37 @@
+﻿using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.IO;
+using Microsoft.Extensions.FileSystemGlobbing;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jellyfin.Plugin.YoutubeMetadata.Providers.LocalMetadata
+{
+    public class YoutubeLocalSeasonProvider : ILocalMetadataProvider<Season>, IHasItemChangeMonitor
+    {
+        public string Name => Constants.PluginName;
+
+        public Task<MetadataResult<Season>> GetMetadata(ItemInfo info, IDirectoryService directoryService, CancellationToken cancellationToken)
+        {
+            MetadataResult<Season> result = new();
+            var item = new Season();
+            item.IndexNumber = 1;
+            item.Name = "Season 1";
+            item.OriginalTitle = "Season 1";
+            result.Item = item;
+            result.HasMetadata = true;
+            return Task.FromResult(result);
+        }
+
+        public bool HasChanged(BaseItem item, IDirectoryService directoryService)
+        {
+
+            return true;
+        }
+    }
+}
