9b4b247564317f13c22554b4bae2ea6e40a9ae22
ankenyr <ankenyr@gmail.com>
2022-06-30 22:21:12 -0700

    Adding in configuration to select between YTDLP and TubeArchivist naming scheme

--- Jellyfin.Plugin.YoutubeMetadata/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.YoutubeMetadata/Configuration/PluginConfiguration.cs
@@ -1,8 +1,24 @@
 using MediaBrowser.Model.Plugins;
+using System;
+
+public enum IDTypes
+{
+    YTDLP = 1,
+    TubeArchivist = 2
+}
 
 namespace Jellyfin.Plugin.YoutubeMetadata.Configuration
 {
+
     public class PluginConfiguration : BasePluginConfiguration
     {
+        public IDTypes IDType { get; set; }
+        public PluginConfiguration()
+        {
+            // defaults
+            IDType = IDTypes.YTDLP;
+        }
+
+
     }
 }
